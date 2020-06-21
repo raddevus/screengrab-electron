78e9f956728b0ea15ea10af8b3de4a5e62c39f38
raddevus <1radeutsch1@gmail.com>
2020-06-21 12:27:17 -0400

    Fixed so screen 2 can also be capture from.  Had to do offset on x value related to width of screen and had to determine that the point is on scree 2 by examining the location in comparison to the width of screen 1. Note: this would not work for a 3rd or 4th screen.

--- app/extra.js
+++ app/extra.js
@@ -33,7 +33,15 @@ function onCapture(evt, targetDir,currentScreen,beginPoint,endPoint) {
         .then(async sources => {
             console.log("in here...")
             console.log(sources)
-            const png = sources[0].thumbnail.crop({x:beginPoint.x,y:beginPoint.y,width:200,height:200}).toPNG()
+            let screenChoice = -1
+            let xOffset = 0;
+            if (beginPoint.x > currentScreen.bounds.width){
+              screenChoice = 1;
+              console.log("screenChoice: " + screenChoice)
+              xOffset = currentScreen.bounds.width;
+            }
+            else{ screenChoice = 0; console.log("screenChoice: " + screenChoice);}
+            const png = sources[screenChoice].thumbnail.crop({x:Math.abs(beginPoint.x-xOffset),y:beginPoint.y,width:200,height:200}).toPNG()
             //const png = currentScreen.thumbnail.crop({x:beginPoint.x,y:beginPoint.y,width:200,height:200}).toPNG()
             //const png = sources[0].thumbnail.toPNG()
             console.log(png);
